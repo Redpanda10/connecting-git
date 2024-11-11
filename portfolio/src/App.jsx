c36d17362f0e3c138f59e7dca2877bfd175519a0
mohitsinghbudal <budalmohitsingh@gmail.com>
2024-11-11 19:24:17 +0545

    hi

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,26 @@
+import './App.css'
+
 export default function App(){
   return<>
-    <div className="header"></div>
+    <div className="main">
+      <div className="navigation">
+      <a href="">About</a>
+      <a href="">Skills</a>
+      <a href="">Projects</a>
+      <a href="">Resume</a>
+      <a href="">Contact me</a>    
+      </div>
+
+      <div className="header-section">
+        <div className='intro'>name here</div>
+        <div className='intro'>image here</div>
+      </div>
+      <div className="about"></div>
+      <div className="skills"></div>
+      <div className="projects"></div>
+      <div className="resume"></div>
+      <div className="contact"></div>
+      <div className="footer"></div>
+  </div>
   </>
 }
