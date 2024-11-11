680e6e0811bbbe84d1f0fa15b32ff778451aa416
mohitsinghbudal <budalmohitsingh@gmail.com>
2024-11-11 18:10:02 +0545

    today new commit with the header part

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,26 @@
+import './App.css'
+
 export default function App(){
   return<>
-    <div className="header"></div>
+  <div className="main-body">
+   <div className="header">
+      <ul>
+        <a>Header</a>
+        <a>About</a>
+        <a>Skill</a>
+        <a>Porjects</a>
+        <a>resume</a>
+        <a>contact</a>
+        <a>footer</a>
+      </ul> 
+    </div>
+    <div className="intro">
+      <div className='column'>Hello this is mohit singh budal</div>
+      <div className='column'>Here is the pic of mine</div> 
+    </div>
+    <div className="about">short bio highaghting your skills and experience</div>
+    <div className="footer">copyright notice and any additional anks or information</div>
+  </div>
+    
   </>
 }
